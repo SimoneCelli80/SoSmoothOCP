6f5271fb678beb9296aec0ca9a903e5d83a81f6c
SimoneCelli80 <158849846+SimoneCelli80@users.noreply.github.com>
2024-09-06 12:49:58 +0200

    Initial commit

---
+++ src/main/java/com/sosmoothocp/app/SoSmoothOcpApplication.java
@@ -0,0 +1,13 @@
+package com.sosmoothocp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoSmoothOcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoSmoothOcpApplication.class, args);
+	}
+
+}
